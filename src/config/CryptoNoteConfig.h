ec5d11ed99491d30ed143e33f0218e331212b081
Dave <57211969+khasicrypto@users.noreply.github.com>
2019-10-31 15:15:54 +0530

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -18,13 +18,13 @@ namespace CryptoNote
 {
     namespace parameters
     {
-        const uint64_t DIFFICULTY_TARGET = 30; // seconds
+        const uint64_t DIFFICULTY_TARGET = 60; // seconds
 
-        const uint32_t CRYPTONOTE_MAX_BLOCK_NUMBER = 500000000;
+        const uint32_t CRYPTONOTE_MAX_BLOCK_NUMBER = 250000000;
 
-        const size_t CRYPTONOTE_MAX_BLOCK_BLOB_SIZE = 500000000;
+        const size_t CRYPTONOTE_MAX_BLOCK_BLOB_SIZE = 250000000;
 
-        const size_t CRYPTONOTE_MAX_TX_SIZE = 1000000000;
+        const size_t CRYPTONOTE_MAX_TX_SIZE = 500000000;
 
         const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 0x29b79d;
 
